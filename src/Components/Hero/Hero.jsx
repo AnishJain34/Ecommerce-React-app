7941701f9a2df09c2c387912165f5019144386fa
Anish <anishjain4470@gmail.com>
2023-11-26 23:32:15 +0530

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='hero-left'>
+<h2>
+    NEWW ARRIVAL ONLY
+</h2>
+<div>
+    
+</div>
+        </div>
+        Hero</div>
+  )
+}
